96f473a1ca12b8f3650387c611652d5c7c7a0d2a
IskandarNabiev <iskandarnabiev96@gmail.com>
2020-03-06 12:54:38 +0300

    Changes to bookmarks

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #
-# LOGIN_REDIRECT_URL = 'dashboard'
-# LOGIN_URL = 'login'
-# LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
