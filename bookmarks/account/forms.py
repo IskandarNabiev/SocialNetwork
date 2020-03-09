ccdb2957d84c83260c4854a1293f92eb3a98aec7
IskandarNabiev <iskandarnabiev96@gmail.com>
2020-03-09 18:08:57 +0300

    Profile edit page

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 class LoginForm(forms.Form):
     username = forms.CharField()
@@ -19,4 +20,18 @@ class UserRegistrationFrom(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
+
+#UserEditForm – позволит пользователям менять имя, фамилию, e-mail (поля встроенной в Django модели);
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+#ProfileEditForm – позволит модифицировать дополнительные сведения, которые мы сохраняем в модели Profile (дату рождения и аватар).
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
+
+
 
