e943cc4fc6e466f67c33f365f20c3dad7065816c
IskandarNabiev <iskandarnabiev96@gmail.com>
2020-03-20 23:37:27 +0300

    Redefining method save() for ImageCreationForm

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from .models import Image
 
+from urllib import request
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+
+
 class ImageCreateForm(forms.ModelForm):
     class Meta:
         model = Image
@@ -9,9 +14,26 @@ class ImageCreateForm(forms.ModelForm):
 
 
     def clean_url(self):
+        #Проверка значения поля url
         url = self.cleaned_data['url']
         valid_extensions = ['jpg', 'jpeg']
         extension = url.rsplit('.', 1)[1].lower()
         if extension not in valid_extensions:
             raise forms.ValidationError("The given URL does not match valid image extensions")
         return url
+
+    #Переопределение метода save
+    def save(self, force_insert=False, force_update=False, commit=True):
+        #Создание объекта image
+        image = super(ImageCreateForm, self).save(commit=True)
+        #получаем URL из атрибута cleaned_data формы
+        image_url = self.cleaned_data['url']
+        #генерируем название изображения, совмещая слаг и расширение картинки
+        image_name = '{} {}'.format(slugify(image.title),
+                                    image_url.rsplit('.', 1)[1].lower())
+        #Скачаиваем изображение по указанному адресу
+        response = request.urlopen(image_url)
+        image.image.save(image_name, ContentFile(response.read()), save=False)
+        if commit:
+            image.save()
+        return image
--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
 
-# Create your views here.
+#Only authorized users have access to image_create
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        #Form is sent
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            #Forms data is valid
+            cd = form.cleaned_data
+            #commit =False, dont save the new object yet
+            new_item = form.save(commit=False)
+            #Adding user to the created object
+            new_item.user = request.user
+            new_item.save()
+            messages.success(request, "Image added successfully")
+            #Redirecting user to the page where image saved
+            return redirect(new_item.get_absolute_url())
+        else:
+            #Fill the form with data from GET request
+            form = ImageCreateForm(data=request.GET)
+        return render(request, 'images/image/create.html', {'section': 'images',
+                                                            'form': form})
